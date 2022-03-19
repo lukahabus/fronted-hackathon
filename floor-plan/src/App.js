da1baa75ba639db3ae01fff5d047d36916fc3b28
Karlo Baljak <karlo.baljak@gmail.com>
2022-03-19 21:29:12 +0100

    We back baby with Stack

--- floor-plan/src/App.js
+++ floor-plan/src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import StackBuild from './Stack.js';
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
+      <Grid item xs={3}>
+        <StackBuild/>
+      </Grid>    
+    </Grid>
   );
 }
 
